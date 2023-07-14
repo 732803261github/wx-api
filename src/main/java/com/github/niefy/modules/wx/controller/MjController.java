d55cd8829dc0b717dc6734561b4546e80bf5d85f
chenxiaoming <732803261@qq.com>
2023-07-14 23:13:12 +0800

    增加查询

--- src/main/java/com/github/niefy/modules/wx/controller/MjController.java
+++ src/main/java/com/github/niefy/modules/wx/controller/MjController.java
@@ -8,7 +8,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -26,8 +25,7 @@ public class MjController {
     public R retrieve_messages(){
         log.info("token:{}",redisTemplate.opsForValue().get("authorization"));
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("authorization",String.valueOf(redisTemplate.opsForValue().get("authorization")));
+        headers.set("authorization", String.valueOf(redisTemplate.opsForValue().get("authorization")));
         HttpEntity requestEntity = new HttpEntity(headers);
         JSONObject response = restTemplate.exchange(
                 "https://discord.com/api/v10/channels/1120568025993715764/messages",

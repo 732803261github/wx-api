d47529a0a781f3dc9da32e4ec3d14e2b598f7641
chenxiaoming <732803261@qq.com>
2023-07-16 02:18:45 +0800

    增加定时任务

--- src/main/java/com/github/niefy/modules/wx/task/ImageFetchTask.java
+++ src/main/java/com/github/niefy/modules/wx/task/ImageFetchTask.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpEntity;
@@ -53,7 +54,7 @@ public class ImageFetchTask {
                 for (Object attachments : ((JSONObject) object).getJSONArray("attachments")) {
                     String taskid = ((JSONObject) object).getString("content").split("]")[0].substring(3);
                     boolean isNumeric = taskid.matches("\\d+");
-                    if (isNumeric) {
+                    if (isNumeric && StringUtils.isNotEmpty(((JSONObject) object).getString("webhook_id"))) {
                         String string = ((JSONObject) attachments).getString("proxy_url");
                         String replace = string.replace("https://media.discordapp.net", "http://www.ai-assistant.com.cn/api/cnd-discordapp");
                         String url = replace + "?Authorization=9998@xunshu";

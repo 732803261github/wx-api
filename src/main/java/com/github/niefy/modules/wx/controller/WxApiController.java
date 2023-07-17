304b43a4965f2096d68b59796ad9d334f86595ca
chenxiaoming <732803261@qq.com>
2023-07-17 18:53:21 +0800

    账号密码redis获取

--- src/main/java/com/github/niefy/modules/wx/controller/WxApiController.java
+++ src/main/java/com/github/niefy/modules/wx/controller/WxApiController.java
@@ -53,19 +53,19 @@ public class WxApiController {
     String appId = "wx0f791e273d673f03";
     String secret = "7a6b5600eb032829c6ac490a1d7bca50";
 
-    public R getWxToken() {
+    public String getWxToken() {
         String URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appId + "&secret=" + secret;
-        if (redisTemplate.opsForValue().get("wxtoken") == null) {
+        if (redisTemplate.opsForValue().get("access_token") == null) {
             ResponseEntity<String> response = restTemplate.getForEntity(URL, String.class);
             if (response.getStatusCodeValue() == 200) {
                 JSONObject jsonObject = JSON.parseObject(response.getBody());
-                redisTemplate.opsForValue().set("wxtoken", response.getBody(), 3600, TimeUnit.SECONDS);
-                return R.ok().put(jsonObject);
+                redisTemplate.opsForValue().set("access_token", response.getBody(), 3600, TimeUnit.SECONDS);
+                return jsonObject.getString("access_token");
             }
         } else {
-            return R.ok().put(JSON.parseObject(redisTemplate.opsForValue().get("wxtoken").toString()));
+            return redisTemplate.opsForValue().get("access_token").toString();
         }
-        return R.ok();
+        return "";
     }
 
 
@@ -84,35 +84,28 @@ public class WxApiController {
         return R.error();
     }
 
-    @GetMapping(value = "/auth")
-    public String oauth(HttpServletResponse response) throws IOException {
+    public R oauth(HttpServletRequest request) throws IOException {
+        String state = UUID.randomUUID().toString();
         String redirectUri = URLEncoder.encode("http://www.ai-assistant.com.cn/wx/auth2code", "UTF-8");
-        String authorizeUrl = String.format("https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_userinfo&state=%s#wechat_redirect", appId, redirectUri, "state");
-        return authorizeUrl;
+        String authorizeUrl = String.format("https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_base&forcePopup=true&state=%s#wechat_redirect", appId, redirectUri, state);
+        return R.ok().put(state);
     }
 
-    @GetMapping(value = "/auth2code")
-    public R invoke(HttpServletRequest request) throws UnsupportedEncodingException {
+    @GetMapping(value = "/code2userinfo")
+    public R code2userinfo(HttpServletRequest request) {
         String code = request.getParameter("code");
-        log.info("auth2code:{}",code);
-        if(StringUtils.isNotEmpty(code)){
-            return R.ok().put(code);
-        }
-        return R.error("授权失败");
-    }
-    @GetMapping(value = "code2userinfo")
-    public R code2userinfo(String code){
-        String url = String.format("https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code",appId,secret,code);
+        log.info("auth2code:{}", code);
+        String url = String.format("https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code", appId, secret, code);
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
         if (response.getStatusCodeValue() == 200) {
             JSONObject jsonObject = JSON.parseObject(response.getBody());
             String access_token = jsonObject.getString("access_token");
             String openid = jsonObject.getString("openid");
             //获取用户信息
-            String userInfoUrl = String.format("https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=zh_CN",access_token,openid);
+            String userInfoUrl = String.format("https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=zh_CN", access_token, openid);
             ResponseEntity<String> response2 = restTemplate.getForEntity(userInfoUrl, String.class);
             JSONObject userJson = JSONObject.parseObject(response2.getBody());
-            log.info("openid为：{}的用户信息是：{}",openid,userJson);
+            log.info("openid为：{}的用户信息是：{}", openid, userJson);
             return R.ok().put(userJson);
         } else {
             return R.error("系统异常");
@@ -138,7 +131,7 @@ public class WxApiController {
     @GetMapping(value = "/pcAuth")
     public R pageAuth() throws UnsupportedEncodingException {
         if (redisTemplate.opsForValue().get("qrcode") == null) {
-            String token = JSON.parseObject(getWxToken().get("data").toString()).getString("access_token");
+            String token = getWxToken();
             String url = String.format("https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=%s", token);
             JSONObject jsonObject = JSON.parseObject("{\"action_name\": \"QR_LIMIT_SCENE\", \"action_info\": {\"scene\": {\"scene_id\": \"pcAuth\"}}}");
             ResponseEntity<String> response = restTemplate.postForEntity(url, jsonObject, String.class);
@@ -189,15 +182,15 @@ public class WxApiController {
     @RequestMapping(value = "/infoSend")
     void sendTemplateMsg() {
         String appid = String.valueOf(redisTemplate.opsForValue().get("appid"));
-        List<WxMpTemplateData> data  = new ArrayList<>();
-        data.add(new WxMpTemplateData("character_string2","3878599093670556"));
-        data.add(new WxMpTemplateData("time3", DateUtils.format(new Date(),"yyyy-MM-dd HH:mm")));
+        List<WxMpTemplateData> data = new ArrayList<>();
+        data.add(new WxMpTemplateData("character_string2", "3878599093670556"));
+        data.add(new WxMpTemplateData("time3", DateUtils.format(new Date(), "yyyy-MM-dd HH:mm")));
         WxMpTemplateMessage wxMpTemplateMessage = WxMpTemplateMessage.builder()
                 .templateId("K_WOhj5KoEgBc7MomCHL4wbq6i82ULsyxDDKepVnZVs")
                 .url("http://ai-assistant.com.cn/api/cnd-discordapp/attachments/1120568025993715764/1129837389020414062/oliverdaniel_3878599093670556_Swans_fish_in_the_lake_during_the_4a97a352-a0ec-47c0-80a2-c6071031a23b.png?Authorization=9998@xunshu")
                 .toUser("o731S6QvW6NlhTkJyGYJNItsu9a8")
                 .data(data)
                 .build();
-        templateMsgService.sendTemplateMsg(wxMpTemplateMessage,appid);
+        templateMsgService.sendTemplateMsg(wxMpTemplateMessage, appid);
     }
 }

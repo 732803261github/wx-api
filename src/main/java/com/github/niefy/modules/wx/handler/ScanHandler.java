02063a3bb10a6bd3cb4b2e8fc0bf80b72d94405a
chenxiaoming <732803261@qq.com>
2023-07-24 10:25:44 +0800

    网页登录

--- src/main/java/com/github/niefy/modules/wx/handler/ScanHandler.java
+++ src/main/java/com/github/niefy/modules/wx/handler/ScanHandler.java
@@ -9,9 +9,11 @@ import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 import me.chanjar.weixin.mp.util.WxMpConfigStorageHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Binary Wang
@@ -22,12 +24,15 @@ public class ScanHandler extends AbstractHandler {
     MsgReplyService msgReplyService;
     @Autowired
     WxUserService wxUserService;
+    @Autowired
+    RedisTemplate redisTemplate;
 
     @Override
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMpXmlMessage, Map<String, Object> map,
                                     WxMpService wxMpService, WxSessionManager wxSessionManager) {
         //扫码事件处理
-        logger.info("用户扫描带参二维码 OPENID:{},事件:{}" + wxMpXmlMessage.getFromUser(), wxMpXmlMessage.getEventKey());
+        logger.info("用户扫描带参二维码 OPENID:{},事件:{},ticket:{}",wxMpXmlMessage.getFromUser(), wxMpXmlMessage.getEventKey(),wxMpXmlMessage.getTicket());
+        redisTemplate.opsForValue().set("ticket::"+wxMpXmlMessage.getTicket(),wxMpXmlMessage.getFromUser(),2, TimeUnit.SECONDS);
         WxUser wxUser = wxUserService.getById(wxMpXmlMessage.getFromUser());
         if (wxUser.isSubscribe()) {
             logger.info("openid:{},订阅状态:{}", wxUser.getOpenid(), wxUser.isSubscribe());
--- src/main/java/com/github/niefy/modules/wx/handler/SubscribeHandler.java
+++ src/main/java/com/github/niefy/modules/wx/handler/SubscribeHandler.java
@@ -32,7 +32,9 @@ public class SubscribeHandler extends AbstractHandler {
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage, Map<String, Object> context, WxMpService wxMpService,
                                     WxSessionManager sessionManager) {
 
-        this.logger.info("新关注用户 OPENID: " + wxMessage.getFromUser() + "，事件：" + wxMessage.getEventKey());
+        this.logger.info("新关注用户 OPENID: {}",wxMessage.getFromUser());
+        logger.info("redis-key:{}","ticket::"+wxMessage.getTicket());
+        logger.info("redis-val:{}",wxMessage.getFromUser());
         redisTemplate.opsForValue().set("ticket::"+wxMessage.getTicket(),wxMessage.getFromUser(),2, TimeUnit.SECONDS);
         String appid = WxMpConfigStorageHolder.get();
         this.logger.info("appid:{}",appid);

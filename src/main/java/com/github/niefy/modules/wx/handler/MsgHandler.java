1a2e43c79a135e9b6af71506d235092017ff341e
chenxiaoming <732803261@qq.com>
2023-08-01 22:05:43 +0800

    gpt自动回复

--- src/main/java/com/github/niefy/modules/wx/handler/MsgHandler.java
+++ src/main/java/com/github/niefy/modules/wx/handler/MsgHandler.java
@@ -1,8 +1,16 @@
 package com.github.niefy.modules.wx.handler;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.github.niefy.config.TaskExcutor;
+import com.github.niefy.modules.wx.entity.MsgReplyRule;
 import com.github.niefy.modules.wx.entity.WxMsg;
 import com.github.niefy.modules.wx.service.MsgReplyService;
 import com.github.niefy.modules.wx.service.WxMsgService;
@@ -11,12 +19,15 @@ import me.chanjar.weixin.mp.util.WxMpConfigStorageHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Binary Wang
@@ -29,12 +40,16 @@ public class MsgHandler extends AbstractHandler {
     @Autowired
     WxMsgService wxMsgService;
     private static final String TRANSFER_CUSTOMER_SERVICE_KEY = "人工";
+    RestTemplate restTemplate = new RestTemplate();
 
     @Override
     public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
                                     Map<String, Object> context, WxMpService wxMpService,
                                     WxSessionManager sessionManager) {
-
+        logger.info("监听到公众号后台用户消息");
+        logger.info("{},{}",wxMessage.getContent(),wxMessage.getContent().substring(0,4).trim().equals("@gpt"));
+        String prompt = wxMessage.getContent().substring(4);
+        askGPT(prompt);
         String textContent = wxMessage.getContent();
         String fromUser = wxMessage.getFromUser();
         String appid = WxMpConfigStorageHolder.get();
@@ -48,6 +63,22 @@ public class MsgHandler extends AbstractHandler {
         }
         return null;
 
+    }
+
+    public String askGPT(String prompt){
+        logger.info(prompt);
+        Map<String, Object> map = new HashMap<>();
+        map.put("prompt",prompt);
+        HttpEntity requestEntity = new HttpEntity(map);
+        String response = restTemplate.exchange(
+                "http://ai-assistant.com.cn:8081/wxcom/message",
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        ).getBody();
+        logger.info("response:{}",response);
+        logger.info("response2:{}",JSON.parseObject(response));
+        return "";
     }
 
 }

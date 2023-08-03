1bce62c9fa5a2ffa92807d6ce4c1a493bfb46d0a
chenxiaoming <732803261@qq.com>
2023-08-03 18:54:52 +0800

    记录使用次数

--- src/main/java/com/github/niefy/modules/wx/controller/OpenaiController.java
+++ src/main/java/com/github/niefy/modules/wx/controller/OpenaiController.java
@@ -3,14 +3,13 @@ package com.github.niefy.modules.wx.controller;
 import com.github.niefy.common.utils.DateUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -25,17 +24,34 @@ public class OpenaiController {
         List<String> keys = Arrays.asList(redisTemplate.opsForValue().get("keys").toString().split(","));
         keys.forEach(key -> {
             String hashKey = DigestUtils.sha1Hex(key);
-            log.info("DigestUtils.sha1Hex({})={}",key,hashKey);
             if (hashKey.equals(apiKey)) {
-                String day = DateUtils.format(new Date(), "yyyy-MM-dd").concat(key);
-                int record = 0;
-                if (redisTemplate.opsForValue().get(day) != null) {
-                    record += Integer.parseInt(redisTemplate.opsForValue().get(day).toString());
-                    redisTemplate.opsForValue().getAndSet(day, record);
-                } else {
-                    redisTemplate.opsForValue().set(day, 1, 30, TimeUnit.DAYS);
-                }
+                String dayUseKey = DateUtils.format(new Date(), "yyyy-MM-dd").concat("||").concat(key);
+                increKey(key,dayUseKey);
+                String userDayUseKey = DateUtils.format(new Date(), "yyyy-MM-dd").concat("||").concat(key);
+                updateUserRecord("USE-".concat(openid), userDayUseKey);
             }
         });
+    }
+
+    public void increKey(String key,String dayUseKey) {
+        if (ObjectUtils.isEmpty(redisTemplate.opsForHash().get(key, dayUseKey))) {
+            Map<String, Object> map = new HashMap<>();
+            map.put(dayUseKey, "1");
+            redisTemplate.opsForHash().putAll(key, map);
+            redisTemplate.opsForHash().getOperations().expire(key, 30, TimeUnit.DAYS);
+        } else {
+            redisTemplate.opsForHash().increment(key, dayUseKey, 1);
+        }
+    }
+
+    public void updateUserRecord(String openid, String key) {
+        if (ObjectUtils.isEmpty(redisTemplate.opsForHash().get(openid, key))) {
+            Map<String, Object> map = new HashMap<>();
+            map.put(key, "1");
+            redisTemplate.opsForHash().putAll(openid, map);
+            redisTemplate.opsForHash().getOperations().expire(openid, 30, TimeUnit.DAYS);
+        } else {
+            redisTemplate.opsForHash().increment(openid, key, 1);
+        }
     }
 }

c7cb471c98beadc50ffe466e099ba21e6250ca02
chenxiaoming <732803261@qq.com>
2023-08-04 17:15:55 +0800

    允许跨域

--- src/main/java/com/github/niefy/config/CorsConfig.java
+++ src/main/java/com/github/niefy/config/CorsConfig.java
@@ -10,9 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*")
-            .allowCredentials(true)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .maxAge(3600);
+                .allowedHeaders("Content-Type","X-Requested-With","accept,Origin","Access-Control-Request-Method","Access-Control-Request-Headers","token")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(true);
     }
 }
